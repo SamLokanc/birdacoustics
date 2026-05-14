1d1b4a1e17c682e8e818baac909791a6f3859670
SamLokanc <samlokanc5@gmail.com>
2026-05-13 17:46:38 -0700

    add documentation to directory creation section

--- src/setup_scratch.sh
+++ src/setup_scratch.sh
@@ -2,9 +2,13 @@
 
 SCRATCH_BASE=/scratch/st-mgmitche-1 #move alloc name to .env?
 
+# ----- Scratch Directory Creation -----
+# Check if a directory matching the pattern exists. If true
+# select the latest version. If false create a new directory
+# with the proper naming convention.
 if compgen -G "${SCRATCH_BASE}/${USER}/${USER}_birdacoustics_*" > /dev/null; then
  SCRATCH_DIR=$(
-  compgen -G "/scratch/st-mgmitche-1/slokanc/slokanc_birdacoustics_*" |
+  compgen -G "${SCRATCH_BASE}/${USER}/${USER}_birdacoustics_*" |
   sort -t_ -k3 -r |
   head -n 1)
 else
