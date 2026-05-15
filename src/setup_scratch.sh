80005b516df1c59331e9a11f2568e791f2f51eb4
SamLokanc <samlokanc5@gmail.com>
2026-05-14 18:24:42 -0700

    remove hawkears setup

--- src/setup_scratch.sh
+++ src/setup_scratch.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+SCRATCH_BASE="/scratch/st-mgmitche-1" #move alloc name to .env?
+
 # ----- Parse Flags -----
 # Flags:
 #  -f : if set the script will setup the scratch directory
-#       regardless of whether or not one from an earlier 
+#       regardless of whether or not one from an earlier
 #       date exists.
+#  -h : indicates that the scratch directory should include
+#       the hawkears container and module files.
 FORCE=0
-while getopts "f" flag; do
+while getopts "fh" flag; do
  case $flag in
   f )
     FORCE=1 ;;
@@ -15,9 +19,7 @@ while getopts "f" flag; do
      exit 1;;
  esac
 done
-shift $((OPTIND-1))
-
-SCRATCH_BASE=/scratch/st-mgmitche-1 #move alloc name to .env?
+shift $(( OPTIND-1 ))
 
 # ----- Scratch Directory Creation -----
 # Check if no directory matching the pattern exists OR if the
@@ -25,10 +27,10 @@ SCRATCH_BASE=/scratch/st-mgmitche-1 #move alloc name to .env?
 # convention: in scratch/user/user_project_YYYYMMDD. If directory(ies)
 # matching pattern already exists select the most recent one based on
 # date in the file name.
-if (! compgen -G "${SCRATCH_BASE}/${USER}/${USER}_birdacoustics_*" > /dev/null) || (( FORCE )); then
+if ( ! compgen -G "${SCRATCH_BASE}/${USER}/${USER}_birdacoustics_*" > /dev/null ) || (( FORCE )); then
  SCRATCH_DIR="${SCRATCH_BASE}/${USER}/${USER}_birdacoustics_$(date +%Y%m%d)"
  echo "Creating ${SCRATCH_DIR} ..."
- mkdir "${SCRATCH_DIR}"
+ mkdir -p "${SCRATCH_DIR}"
  echo "Done."
 else
  SCRATCH_DIR=$(
@@ -37,3 +39,4 @@ else
   head -n 1)
  echo "${SCRATCH_DIR} already exists."
 fi
+
