1d06f030eee42827986b418d7c1858072bf3c2b5
SamLokanc <samlokanc5@gmail.com>
2026-05-19 11:40:44 -0700

    add scaffolding for file conversion

---
+++ src/convert_kaleidoscope.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ----- Parse Arguments -----
+# Flags:
+#  -k : Path to Kaleidoscope.sif file
+#  -s : Path to settings.ini file
+while getopts "o:i:d:" flag; do
+  case $flag in
+    k) KALEIDOSCOPE="$OPTARG" ;;
+    s) SETTINGS="$OPTARG" ;;
+    \?) echo "Invalid option: -$OPTARG, exiting..." >&2; exit 1 ;;
+  esac
+done
+shift $(( OPTIND - 1 ))
+
+# ----- Run Kaleidoscope File Conversion -----
+apptainer \
+ exec KALEIDOSCOPE \
+ kaleidoscope-cli \
+ --batch SETTINGS
