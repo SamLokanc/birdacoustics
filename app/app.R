b10e02607066a9b9a0d111114ca1388059bfa634
SamLokanc <SamL.5@icloud.com>
2026-05-22 20:01:29 -0700

    add sidebar and filters scaffolding

--- app/app.R
+++ app/app.R
@@ -1,7 +1,48 @@
 library(shiny)
 library(bslib)
 
-ui <- page_fluid()
+ui <- page_fluid(
+  # ----- Sidebar -----
+  page_sidebar(
+    sidebar = sidebar(
+      # ----- Filters -----
+      strong("Filters"),
+      input_switch(
+        "merged_filter", 
+        "Merge Adjacent",
+      ),
+      selectizeInput(
+        "location_filter",
+        "Location",
+        list("Recorder 1" = "1", "Recorder 2" = "2", "Recorder 3" = "3"),
+        multiple = TRUE,
+      ),
+      selectizeInput(
+        "species_filter",
+        "Species",
+        list("Species 1" = "1", "Species 2" = "2", "Species 3" = "3"),
+        multiple = TRUE,
+      ),
+      dateRangeInput(
+        inputId = "date_filter", 
+        label = "Date Range"
+      ),
+      sliderInput(
+        "confidence_filter", 
+        "Confidence Threshold",
+        min = 0,
+        max = 1,
+        value = 0,
+      ),
+      
+      
+      hr(), # section break
+    )
+  ),
+  
+  # ----- Main Page -----
+  
+)
 
 server <- function(input, output, session){}
 
