6d272fa551cb17c0d02787cdfe7d81c3bcbdc30b
SamLokanc <samlokanc5@gmail.com>
2026-05-28 11:20:18 -0700

    add force to submit.sh

--- src/00-submit.sh
+++ src/00-submit.sh
@@ -2,9 +2,27 @@
 
 set -euo pipefail
 
+# ----- Parse Flags -----
+# Flags:
+#  -f : Force the creation of the scratch directory. Since
+#       everything exists in the scratch directory this will
+#       essentially force a new project instance to be created.
+FORCE=0
+while getopts "f" flag; do
+ case $flag in
+  f) FORCE=1 ;;
+  \?) echo "ERROR: Invalid option, exiting..." >&2; exit 1;;
+ esac
+done
+shift $(( OPTIND-1 ))
+
 # ----- Set Scratch Directory -----
 export SCRATCH_BASE="/scratch/st-mgmitche-1" #move alloc name to .env?
-export SCRATCH=$(bash src/01-setup_scratch.sh)
+if [ "${FORCE}" -eq 1 ]; then
+ export SCRATCH=$(bash src/01-setup_scratch.sh -f)
+else
+ export SCRATCH=$(bash src/01-setup_scratch.sh)
+fi
 unset SCRATCH_BASE
 
 # ----- Set Project Directory -----
