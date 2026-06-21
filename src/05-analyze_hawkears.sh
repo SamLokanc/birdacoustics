033f5c87f59959814a19484e37389fba856f6e98
SamLokanc <samlokanc5@gmail.com>
2026-06-21 11:56:39 -0700

    update to no longer take date as an argument

--- src/05-analyze_hawkears.sh
+++ src/05-analyze_hawkears.sh
@@ -8,11 +8,10 @@ set -euo pipefail
 #  -i : input. Takes path to input directory as an argument.
 #  -d : date. Takes date of recording as argument.
 #  -t : number of threads. Takes ${SLURM_CPUS_PER_TASK} as argument.
-while getopts "o:i:d:t:" flag; do
+while getopts "o:i:t:" flag; do
   case $flag in
     o) OUTPUT="$OPTARG" ;;
     i) INPUT="$OPTARG" ;;
-    d) DATE="$OPTARG" ;;
     t) THREADS="$OPTARG" ;;
     \?) echo "ERROR: Invalid option: -$OPTARG, exiting..." >&2; exit 1 ;;
   esac
@@ -28,7 +27,7 @@ analyze.py \
  -i "${INPUT}" \
  -o "${OUTPUT}" \
  -m 0 \
- --date "${DATE}" \
+ --date file \
  --lat 49.250 \
  --lon -123.236 \
  --threads "${THREADS}" \
